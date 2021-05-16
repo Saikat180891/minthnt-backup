f82bfdf028c900fd219ec2c02c597daa47d1943d
saikat180891 <saptadipa123@gmail.com>
2021-05-17 01:07:31 +0530

    Made mods

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -3,10 +3,23 @@ import { createGlobalStyle } from "styled-components";
 import tw, { theme, GlobalStyles as BaseStyles } from "twin.macro";
 
 const CustomStyles = createGlobalStyle`
-  body {
-    -webkit-tap-highlight-color: ${theme`colors.purple.500`};
-    ${tw`antialiased`}
-  }
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+
 `;
 
 const GlobalStyles = () => (
