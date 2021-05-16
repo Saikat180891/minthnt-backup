6a87e2882864dd6c781da5809f8fa437e1db4c06
saikat180891 <saptadipa123@gmail.com>
2021-05-17 00:33:39 +0530

    Added tailwind

---
+++ .babelrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [["next/babel", { "preset-react": { runtime: "automatic" } }]],
+  plugins: [
+    "babel-plugin-macros",
+    "inline-react-svg",
+    ["styled-components", { ssr: true }],
+  ],
+};
---
+++ src/pages/dashboard.js
@@ -0,0 +1,7 @@
+import tw from "twin.macro";
+
+const DashboardPage = () => {
+  return <div tw="bg-green-500">DashboardPage</div>;
+};
+
+export default DashboardPage;
