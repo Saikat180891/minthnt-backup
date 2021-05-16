afb1d894923d671d79ba1b81cdd494cca470fc3b
saikat180891 <saptadipa123@gmail.com>
2021-05-17 01:39:20 +0530

    changed webpack from 5 to 4

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  future: { webpack5: true },
-  webpack: (config) => {
-    // Unset client-side javascript that only works server-side
-    config.resolve.fallback = { fs: false, module: false };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Unset client-side javascript that only works server-side
+      // https://github.com/vercel/next.js/issues/7755#issuecomment-508633125
+      config.node = { fs: "empty", module: "empty" };
+    }
 
     return config;
   },
